64f2d37490a72b9a67b7f23d9ca2e1c996438f0a
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 22:29:27 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,5 +1,9 @@
+local_greeting = Hey
+other_name = Chen
+my_name = Zoe
+time_of_day = Afternoon
 def greeting
-  puts "Hi Chen, I'm Zoe, how's your afternoon?"
+  puts "#{local_greeting} #{other_name}, I'm #{my_name}. How's your #{time_of_day}?"
 end
 
 greeting

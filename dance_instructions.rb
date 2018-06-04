bd0c06cc942ca0d6099285078ce0a75b40b07251
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 22:08:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -14,8 +14,9 @@ def Bob_The_Reins_And_Skip-Steps_Eight_Times
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+  puts ""
 end
 def Lasso_And_Skip-Steps_Eight Times
  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
  puts "Lift right arm up and point right elbow diagonally to the right."
- puts ""
+ puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."

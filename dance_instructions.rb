e3cc59e4caacd29aed82443cf584253684a20d5d
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 22:03:03 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,13 @@
-
-# Code your methods below
+def Assume Stance
+  puts 
+end
+def Bust A Move
+  puts
+end
+def Footwork
+  puts
+end
+def Bob The Reins And Skip-Steps Eight Times 
+  puts
+end
+def Lasso And Skip-Steps Eight Times

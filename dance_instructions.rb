3c68dc94d79ae3c3e0d6cf7624a0a84c459328f1
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 22:10:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -21,6 +21,7 @@ def Skip_Step_Left
   puts "eft, Right, Left and Left"
 end
 def Skip_Step_Right
+  puts "Lower right foot to the ground"
   puts ""
 def Lasso_And_Skip-Steps_Eight Times
  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."

957fe1c222b22a4b427ae34d4a751c0b6a8d3ba6
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 22:17:48 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,53 +1,80 @@
 def Assume_Stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end
+
 def Bust_A_Move
   puts "Be cool"
   puts "Be snazzy!"
 end
+
 def Footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
   puts "Finish with a small skip-step backward"
 end
+
 def Bob_The_Reins 
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
 end
+
 def Skip_Step_Left
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
   puts "eft, Right, Left and Left"
 end
+
 def Skip_Step_Right
   puts "Lower right foot to the ground"
   puts "Bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
 end
+
 def Lasso
  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
  puts "Lift right arm up and point right elbow diagonally to the right."
  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
  end
+ 
  Assume_Stance
+ 
  Bust_A_Move
+ 
  Footwork
+ 
  Bob_The_Reins
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Lasso
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
+ 
  Skip_Step_Right
+ 
  Skip_Step_Left
